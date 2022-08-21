2762a9a9f80501b4225bcec6a6617fbfbd78a5ae
Cesar Lopez Pastrana <ceslopast@gmail.com>
2022-08-21 17:16:27 +0200

    grad. Command line arguments, python script improved and generated examples

--- gradient_descendent/src/main.cpp
+++ gradient_descendent/src/main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
+
 #include "thomson.hpp"
 
 
 
-int main()
+
+
+int main(int argc, char *argv[])
 {
+     
+    if(argc <3) {
+        printf("Not sufficient parameters. Usage thomson n_particles radius\n");
+        exit(1);
+    }
+ 
+    int N = atoi(argv[1]);                  /* Number of particles */
+    double R = atof(argv[2]);              /* Radius */
     
-    int N;
-    double R;
-    std::cout << "Set number of particles: "; std::cin >> N;
-    std::cout << "Set sphere radius: "; std::cin >> R;
-    
+    std::cout << "Minimising... ";
     Particles surface(N, R);
     surface.minimise(100, 1.0e-6);
-    Export dump(&surface, "./output/");
-    dump.exportXYZ("sphere_minimised");
+    Export dump(&surface, "./");
+    dump.exportDAT("rsphere_eq");
+    
+    std::cout << "Done!" << std::endl;
     return 0;
 }
 
